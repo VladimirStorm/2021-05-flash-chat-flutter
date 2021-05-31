be92fd5655dead279cc512c605bf2b2731091a6c
FlutLab.io <>
2021-05-31 06:55:27 +0000

    2021-05-31 office

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart'; //2021-05-31 11-31
+
 //2021-05-30 залил проект на github.!!!!
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen'; //добавили в перем-ю static
@@ -9,8 +11,7 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen>
-    with SingleTickerProviderStateMixin {
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin {
   //добавляем Mixin
   AnimationController controller;
   @override
@@ -33,55 +34,52 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor:
-            Colors.white, //вернём знач-е на white, т.к. мы поменяли
+        backgroundColor: Colors.white, //вернём знач-е на white, т.к. мы поменяли
         //верхнюю границу счётчика до 100, а opacity не может быть больше 1.
         body: Padding(
             padding: EdgeInsets.symmetric(horizontal: 24.0),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Row(children: <Widget>[
-                    Hero(
-                        //добавили Hero Animation.
-                        tag: 'logo',
-                        child: Container(
-                          child: Image.asset('images/logo.png'),
-                          height: controller.value, //теперь добавим анимацию увеличения лого
-                        )),
-                    Text('Flash chat', //вернём начал-е значение текста
-                        style: TextStyle(
-                            fontSize: 45.0, fontWeight: FontWeight.w900))
-                  ]),
-                  SizedBox(height: 48.0),
-                  Padding(
-                      padding: EdgeInsets.symmetric(vertical: 16.0),
-                      child: Material(
-                          elevation: 5.0,
-                          color: Colors.lightBlueAccent,
-                          borderRadius: BorderRadius.circular(30.0),
-                          child: MaterialButton(
-                              onPressed: () {
-                                Navigator.pushNamed(context, LoginScreen.id);
-                              },
-                              minWidth: 200.0,
-                              height: 42.0,
-                              child: Text('Log In')))),
-                  Padding(
-                      padding: EdgeInsets.symmetric(vertical: 16.0),
-                      child: Material(
-                          color: Colors.blueAccent,
-                          borderRadius: BorderRadius.circular(30.0),
-                          elevation: 5.0,
-                          child: MaterialButton(
-                              onPressed: () {
-                                Navigator.pushNamed(
-                                    context, RegistrationScreen.id);
-                              },
-                              minWidth: 200.0,
-                              height: 42.0,
-                              child: Text('Register'))))
-                ])));
+            child: Column(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.stretch, children: <Widget>[
+              Row(children: <Widget>[
+                Hero(
+                    //добавили Hero Animation.
+                    tag: 'logo',
+                    child: Container(
+                      child: Image.asset('images/logo.png'),
+                      height: controller.value, //теперь добавим анимацию увеличения лого
+                    )),
+                TypewriterAnimatedTextKit(
+                    text: [
+                      'Flash Chat'
+                    ], //2021-05-31 11-31
+                    style: TextStyle(fontSize: 45.0, fontWeight: FontWeight.w900))
+              ]),
+              SizedBox(height: 48.0),
+              Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                  child: Material(
+                      elevation: 5.0,
+                      color: Colors.lightBlueAccent,
+                      borderRadius: BorderRadius.circular(30.0),
+                      child: MaterialButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, LoginScreen.id);
+                          },
+                          minWidth: 200.0,
+                          height: 42.0,
+                          child: Text('Log In')))),
+              Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                  child: Material(
+                      color: Colors.blueAccent,
+                      borderRadius: BorderRadius.circular(30.0),
+                      elevation: 5.0,
+                      child: MaterialButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, RegistrationScreen.id);
+                          },
+                          minWidth: 200.0,
+                          height: 42.0,
+                          child: Text('Register'))))
+            ])));
   }
 }
